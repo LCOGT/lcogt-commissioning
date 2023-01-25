7f4e13c0ecb2d24221c4814b1a2621bb5ca6e2eb
Daniel Harbeck <drhaz@users.noreply.github.com>
2023-01-25 13:24:20 -0800

    version bump vor qhy600 adjustments

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lcocommissioning",
-    version="2.1.2",
+    version="2.1.3",
     author="Daniel Harbeck",
     author_email="dharbeck@lco.global",
     description="Tool to characterize CCD detectors and other commissioning tasks for the LCO observatory.",
