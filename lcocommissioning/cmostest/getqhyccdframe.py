6aad701f2c6c5768742dd157a44ba3cd08c92d70
Daniel Harbeck <drhaz@users.noreply.github.com>
2023-05-02 13:57:01 -0700

    function generator accessibe via dhcp

--- lcocommissioning/cmostest/getqhyccdframe.py
+++ lcocommissioning/cmostest/getqhyccdframe.py
@@ -249,7 +249,7 @@ def main():
         lab = LED_Illuminator()
 
     for exptime in args.exptime:
-        _logger.info(f"takeing exposures for exptie {exptime}")
+        _logger.info(f"taking exposures for exptime {exptime}")
         for ii in range(args.expcnt):
             imagename = f"{args.outputpath}/qhytest-{datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%S')}.{suffix}.fits"
             if args.flat and lab is not None:
@@ -262,7 +262,7 @@ def main():
                     # Here we open the shutter, and then turn the LED on for a determined amount of time. it takes a few seconds from requesting an exposure
                     # until the shutter actually opens. Hence we are putting the LED con command into a background thread that starts its working day with sleeping.
 
-                    _logger.info (f"Starting frequencey generator defined exposure for {args.nburstcycles} cycles.")
+                    _logger.info (f"Starting frequency generator defined exposure for {args.nburstcycles} cycles.")
                     th =threading.Thread ( target=lab.expose_burst, kwargs={'exptime':exptime, 'ncycles':args.nburstcycles, 'overhead':7, 'voltage':args.ledvoltage, 'block':False})
                     th.start()
 
