8c030bfff5838511da8a8d54978254461c068bde
Daniel Harbeck <drhaz@users.noreply.github.com>
2023-06-23 13:27:25 -0700

    better labels etc for elongation wind plots

--- lcocommissioning/cdk/ana_search.py
+++ lcocommissioning/cdk/ana_search.py
@@ -15,14 +15,14 @@ logging.getLogger('opensearch').setLevel(logging.WARNING)
 logging.getLogger('connectionpool').setLevel(logging.WARNING)
 
 
-def query_opensearch(opensearch_url='https://opensearch.lco.global', index='fitsheaders'):
+def query_opensearch(opensearch_url='https://opensearch.lco.global', index='fitsheaders', site='cpt',enc='aqwa',instrument='sq38'):
     client = OpenSearch(opensearch_url)
 
     sourcecolumn = ['ALTITUDE', 'L1FWHM', 'L1ELLIP', 'AZIMUTH', 'DATE-OBS', 'ORIGNAME', 'FILTER',
                     'WINDSPEE', 'WINDDIR', 'INSTRUME', 'EXPTIME', 'L1ELLIPA',
                     'RA', 'DEC', 'LST', ]
 
-    query = 'INSTRUME:sq30 AND SITEID:ogg AND ENCID:clma AND _exists_:L1FWHM AND _exists_:L1ELLIP'
+    query = f'INSTRUME:{instrument} AND SITEID:{site} AND ENCID:{enc} AND _exists_:L1FWHM AND _exists_:L1ELLIP'
     body = {
         'size': 10000,
         "_source": sourcecolumn,
@@ -62,27 +62,32 @@ def query_opensearch(opensearch_url='https://opensearch.lco.global', index='fits
     return t
 
 
-def plotthings(data):
+def plotthings(data,site,enc,instrumet):
     plt.clf()
     plt.plot(t['DATE-OBS'], t['L1ELLIP'], '.')
     plt.ylim([0, 1])
     plt.savefig('date_el.png')
 
     plt.clf()
-    plt.plot(t['WINDSPEE'], t['L1ELLIP'], '.')
+    plt.scatter(t['WINDSPEE'], t['L1ELLIP'], c=t['WINDDIR'], s=2)
     plt.xlabel("Wind Speed")
     plt.ylabel("Ellipticity")
+    plt.colorbar(label='Wind Direction [\deg]')
     plt.ylim([0, 1])
-    plt.savefig('windspee_el.png')
+    plt.title (f'{site}-{enc}-{instrumet}')
+
+    plt.savefig(f'{site}-{enc}-{instrumet}-windspee_el.png',bbox_inches='tight')
 
     plt.clf()
 
-    plt.plot(t['WINDDIR'], t['L1ELLIP'], '.')
+    plt.scatter(t['WINDDIR'], t['L1ELLIP'], c=t['WINDSPEE'], s=2)
 
     plt.xlabel("Wind Direction")
     plt.ylabel("Ellipticity")
+    plt.colorbar(label='Windspeed')
     plt.ylim([0, 1])
-    plt.savefig('winddir_el.png')
+    plt.title (f'{site}-{enc}-{instrumet}')
+    plt.savefig(f'{site}-{enc}-{instrumet}-winddir_el.png',bbox_inches='tight')
 
     plt.clf()
     plt.plot(t['EXPTIME'], t['L1ELLIP'], '.')
@@ -98,7 +103,7 @@ def plotthings(data):
     plt.ylabel("Ellipticity")
     plt.ylim([0, 1])
 
-    plt.savefig('ellipa_el.png')
+    plt.savefig(f'{site}-{enc}-{instrumet}-ellipa_el.png')
 
     plt.clf()
     plt.plot(t['DATE-OBS'], t['L1ELLIPA'], '.')
@@ -134,6 +139,12 @@ def plotthings(data):
     plt.savefig('az-el.png')
 
 
-t = query_opensearch()
 
-plotthings(t)
+site = 'ogg'
+enc='clma'
+instrument='sq30'
+    
+
+t = query_opensearch(enc=enc,site=site,instrument=instrument)
+
+plotthings(t,enc=enc,site=site,instrumet=instrument)
